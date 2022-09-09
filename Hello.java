c4e5d31f3a3728a492ef68a3501f805c2c8319bb
chris-carter-cs <chris.carter.cs@gmail.com>
2022-09-08 22:19:06 -0400

    Cleaned up code with import

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Dr. Fenwick!.");
-        java.util.Scanner keyboard = new java.util.Scanner(System.in);
+        Scanner keyboard = new java.util.Scanner(System.in);
         System.out.println("How are you?");
         String response = keyboard.nextLine();
     }
